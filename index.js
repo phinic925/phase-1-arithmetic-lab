c8024a237ae1846e489646190e791afcf082fd18
phinic925 <phinicnyangate480@gmail.com>
2022-06-07 17:12:22 +0100

    commit tree fc0100439cfaee7a1b746470c2d980eb7fb089ed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 // Write your code here
 let multiply ;
-let num1 = 36;
+let num1 = 31;
 let num2 = 2;
 multiply = num1 * num2;
+let random ;
+random = Math.ceil(math.random()*10);
+let mod;
+let num3= 19;
+let num4 =5;
+mod =num3 % num4;
+let max;
+max = math.max (10,20,8);
