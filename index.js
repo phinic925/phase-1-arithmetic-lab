e2c85f9568fb33131d5773be91a3baf07ffe1253
phinic925 <phinicnyangate480@gmail.com>
2022-06-07 17:19:47 +0100

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-
+const num1 = 62;
+const num2 = 1;
+const num3 = 19;
+const num4 = 5;
 // Write your code here
-let multiply ;
-let num1 = 36;
-let num2 = 2;
-multiply = num1 * num2;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 10);
+const mod = num3 % num4;
+const max = Math.max(3, 1, 20, 8);
